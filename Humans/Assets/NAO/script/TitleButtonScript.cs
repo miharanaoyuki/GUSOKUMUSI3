4bc6e1e57e5b6bdf60a3b88d32d2f801968653eb
miharanaoyuki <89573107+miharanaoyuki@users.noreply.github.com>
2022-10-19 01:17:09 +0900

    mihara
    ステージ選択・クリアシーンそれぞれに、タイトルへ移行するシーン切り替えを追加&動作確認

--- Humans/Assets/NAO/script/TitleButtonScript.cs
+++ Humans/Assets/NAO/script/TitleButtonScript.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class TitleButtonScript : MonoBehaviour
+public class TitleButtonScript: MonoBehaviour
 {
     public void OnClickStartButton()
     {
