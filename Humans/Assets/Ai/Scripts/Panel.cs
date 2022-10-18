e0f1234764bcd4b33f770dbcc00747946176228e
Fldia <91875240+Fldia@users.noreply.github.com>
2022-10-18 13:03:52 +0900

    ai

---
+++ Humans/Assets/Ai/Scripts/Panel.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panel : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //マウスの左ボタンが押されていたら
+        if(Input.GetMouseButton(0))
+        {
+
+        }
+        //マウスの左ボタンが離されたら
+        if (Input.GetMouseButtonUp(0))
+        {
+
+        }
+    }
+}
