019e98c275a0f86d802526244c9db88159e4935a
Fldia <91875240+Fldia@users.noreply.github.com>
2022-10-20 16:34:04 +0900

    Ai

--- Humans/Assets/Ai/Scripts/Panel.cs
+++ Humans/Assets/Ai/Scripts/Panel.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Panel : MonoBehaviour
 {
+    //マウスの座標を取得させたい
+    //bool on_Flag = true;
+    //bool select_flag = true;
 
     // Start is called before the first frame update
     void Start()
@@ -14,15 +17,19 @@ public class Panel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //マウスの左ボタンが押されていたら
-        if(Input.GetMouseButton(0))
-        {
+        Vector3 mousePos = Input.mousePosition;
 
+        //マウスの左ボタンが押されていたら
+        if (Input.GetMouseButton(0))
+        {
+            //Vector3 mousePos = Input.mousePosition;
         }
         //マウスの左ボタンが離されたら
         if (Input.GetMouseButtonUp(0))
         {
 
         }
+
+        Debug.Log("x:" + mousePos.x + " y:" + mousePos.y);
     }
 }
